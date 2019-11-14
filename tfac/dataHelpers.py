208932384ba5d570c2156c0ad21cdd363e96a941
robthemike1 <43423961+robthemike1@users.noreply.github.com>
2019-11-14 10:28:16 -0800

    Data Export (#12)
    * started function writing
    
    * deleted python notebook
    
    * merge fix
    
    * deleted python notebook
    
    * Added function to filter datasets with unique common genes
    
    * changed synapse id's
    
    * added docstring and removed unnecessary input
    
    * fixed username and pass
    
    * Fixed doc
    
    * Filter by Cell Line
    
    * Cell Line Data
    
    * Filtering func updated
    
    * fixed syn files
    
    * Export Function
    
    * Linting

--- tfac/dataHelpers.py
+++ tfac/dataHelpers.py
@@ -1,10 +1,10 @@
 '''Contains function for importing data from and sending data to synapse'''
 
 import pandas as pd
-from synapseclient import Synapse
+from synapseclient import Synapse, Project, Folder, File, Link
 
 
-def importData(username, password, data=None):
+def importData(username, password, dataType=None):
     '''Data Import from synapse
     ----------------------------------------------
     Parameters:
@@ -21,9 +21,9 @@ def importData(username, password, data=None):
     '''
 
     ## Input Checking
-    if data is None:
+    if dataType is None:
         print('Invalid Data Set')
-        print('Enter:', 'Copy Number,', 'Methylation,', 'or Gene Expression')
+        print('Enter:', 'Copy Number All,', 'Methylation All,', 'or Gene Expression All')
         return None
     syn = Synapse()
     try:
@@ -33,13 +33,35 @@ def importData(username, password, data=None):
         return None
 
     ## Find Data
-    if data == 'Copy Number':
-        data = syn.get('syn21033823')
-    elif data == 'Methylation':
-        data = syn.get('syn21033929')
-    elif data == 'Gene Expression':
-        data = syn.get('syn21033805')
+    if data == 'Copy Number All':
+        data = syn.get('syn21089502')
+    elif data == 'Methylation All':
+        data = syn.get('syn21089540')
+    elif data == 'Gene Expression All':
+        data = syn.get('syn21089539')
 
-    df = pd.read_excel(data.path)
+    df = pd.read_excel(data.path, index_col = 0)
     syn.logout()
     return df
+
+def exportData(username, password, data, nm):
+    '''Pandas Data Frame to upload back to synapse as an excel file
+    ---------------------------------------------------------------
+    Parameters:
+        username: String
+            Your synapse username
+        password: String
+            You synapse password
+        data: Data Frame
+            Pandas object containing the data to upload to synapse as an excel file
+        name: String
+            A name for the file in synapse
+    
+    '''
+    syn = Synapse()
+    syn.login(username, password)
+    proj = syn.get('syn21032722')
+    data.to_csv('data/file.csv')
+    syn.store(File(path='file.csv', name=nm, parent=proj))
+    syn.logout()
+    return
--- tfac/dataOrg.py
+++ tfac/dataOrg.py
@@ -1,28 +1,128 @@
-'''Functions for handling gene alignment'''
 import numpy as np
 import pandas as pd
+from functools import reduce
+from dataHelpers import importData
 
-def extractData(filename, columns=None, row=0, col=None):
-    '''Pullling Data from excel file on server'''
-    return pd.read_excel(filename, header=row, index_col=col, usecols=columns)
+def extractData(filename, columns = None, row = 0, col = None):
+    return pd.read_excel(filename, header = row, index_col = col, usecols = columns)
 
-def extractCopy(dupes=False):
-    '''
-    Extracts out all duplicates data using excel file of gene names
+def extractGeneNames():
+    ''' 
+    Extracts sorted gene names from all data sets
 
     Returns:
             Order: Methylation, Gene Expression, Copy Number
-            List of length 3 containing 3D arrays with
-            duplicate gene names, indices, and # of dupes corresponding to each name
-            Also returns # of duplicates in each data set
+            Returns three numpy arrays with gene names from aforementioned datasets
     '''
     data = extractData('data/GeneData_All.xlsx', 'A:C')
     data = data.to_numpy()
 
-    methylation = np.append(data[:12158, 0], data[12159:21338, 0])
-    geneExp = data[:, 1]
-    copyNum = data[:23316, 2]
-    data = [methylation.astype(str), geneExp.astype(str), copyNum.astype(str)]
+    methylation = np.append(data[:12158,0],data[12159:21338,0]).astype(str)
+    geneExp = data[:,1].astype(str)
+    copyNum = data[:23316,2].astype(str)
+
+    return methylation, geneExp, copyNum
+
+def extractCellLines():
+    ''' 
+    Extracts sorted cell lines from all data sets
+
+    Returns:
+            Order: Methylation, Gene Expression, Copy Number
+            Returns three numpy arrays with cell lines from aforementioned datasets
+    '''
+    data = extractData('data/CellLines_All.xlsx', 'A:C')
+    data = data.to_numpy()
+
+    methylation = data[:843, 0].astype(str)
+    geneExp = data[:1019, 1].astype(str)
+    copyNum = data[:, 2].astype(str)
+
+    return methylation, geneExp, copyNum
+
+def findCommonGenes():
+    '''
+    Finds the set of unique gene names from the copy number, methylation, and gene expression dataset
+
+    Returns:
+            Numpy array of unique common gene names
+    '''
+    methylation, geneExp, copyNum = extractGeneNames()    
+    commonGenes = reduce(np.intersect1d, (methylation, geneExp, copyNum))    
+    return commonGenes
+
+def findCommonCellLines():
+    '''
+    Finds the set of unique cell lines from the copy number, methylation, and gene expression dataset
+
+    Returns:
+            Numpy array of unique common cell lines
+    '''
+    methylation, geneExp, copyNum = extractCellLines()    
+    commonCellLines = reduce(np.intersect1d, (methylation, geneExp, copyNum))    
+    return commonCellLines
+
+def filterData():
+    '''
+    Pushes the filtered data to synapse :D
+    '''
+    methData = importData('NilayShah', 'nilayisthebest', 'Methylation All')
+    geneData = importData('NilayShah', 'nilayisthebest', 'Gene Expression All')
+    copyData = importData('NilayShah', 'nilayisthebest', 'Copy Number All')
+
+    methValues = np.array(methData.values)
+    geneValues = np.array(geneData.values)
+    copyValues = np.array(copyData.values)
+
+    methIdx = np.array(methData.index)
+    geneIdx = np.array(geneData.index)
+    copyIdx = np.array(copyData.index)
+
+    methG, geneG, copyG = extractGeneNames()
+    methCL, geneCL, copyCL = extractCellLines()
+    commonG = findCommonGenes()
+    commonCL = findCommonCellLines()
+
+    # Find indices of common genes in full dataset
+    methGIndices = np.where(np.in1d(methG, commonG))[0]
+    geneGIndices = np.where(np.in1d(geneG, commonG))[0]
+    copyGIndices = np.where(np.in1d(copyG, commonG))[0]
+
+    # Find indices of common cell lines in full dataset
+    methCLIndices = np.where(np.in1d(methCL, commonCL))[0]
+    geneCLIndices = np.where(np.in1d(geneCL, commonCL))[0]
+    copyCLIndices = np.where(np.in1d(copyCL, commonCL))[0]
+
+
+    methFiltered = methValues[methGIndices, methCLIndices]
+    geneFiltered = geneValues[geneGIndices, geneCLIndices]
+    copyFiltered = copyValues[copyGIndices, copyCLIndices]
+
+    methDF = pd.DataFrame(data = methFiltered, index = methIdx[methGIndices], columns = commonCL)
+    geneDF = pd.DataFrame(data = geneFiltered, index = geneIdx[geneGIndices], columns = commonCL)
+    copyDF = pd.DataFrame(data = copyFiltered, index = copyIdx[copyGIndices], columns = commonCL)
+
+
+
+    # Use synapse.store with file and activity functions to upload filtered data to synapse
+
+
+def extractCopy(dupes = False, cellLines = False):
+    ''' 
+    Extracts out all duplicates data using excel file of gene names
+
+    Returns:
+            Order: Methylation, Gene Expression, Copy Number
+            List of length 3 containing 3D arrays with 
+            duplicate gene names, indices, and # of dupes corresponding to each name
+            Also returns # of duplicates in each data set
+    '''
+    if cellLines:
+        methylation, geneExp, copyNum = extractCellLines()
+    else:
+        methylation, geneExp, copyNum = extractGeneNames()
+
+    data = [methylation,geneExp,copyNum]
 
     if dupes:
         duplicates = np.zeros(3)
@@ -42,8 +142,11 @@ def extractCopy(dupes=False):
                 copyData.append(uData[0][j])
                 idxData.append(uData[1][j])
                 count.append(uData[2][j])
-        returnVal.append(np.array([copyData, idxData, count]))
+        returnVal.append(np.array([copyData,idxData,count]))
 
     if dupes:
         return returnVal, duplicates
-    return returnVal
+    else:
+        return returnVal
+
+#want to slice into array to create new datafile using indices

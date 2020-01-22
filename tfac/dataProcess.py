4f1e810c3853e0689e38370b0f3a049169a223a0
neharajkumar <46552058+neharajkumar@users.noreply.github.com>
2020-01-22 12:13:24 -0800

    Workflow func (#45)
    * Created work flow function
    
    * Added work flow function to DataProcess
    
    * Deleted some notebooks

--- tfac/dataProcess.py
+++ tfac/dataProcess.py
@@ -1,60 +1,31 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import scale
+import tqdm
+from synapseclient import Synapse, File
+from dataOrg import filterData
+from fixCutDataScript import cutMissingValues
 
 
-def cutMissingValues(data, threshold):
-    ''' Function takes in data and cuts rows & columns
-    that have more missing values than the threshold set.
-
-    Inputs: data to be cut, threshold to keep (as a fraction)
-
-    Returns: cut data set '''
-
-    uncutData = data
-    rows = uncutData.index
-    cols = uncutData.columns
-
-    uncutData = pd.DataFrame.to_numpy(data)
-    #uncutData = uncutData1.values
-
-    cutData = uncutData
-    data_size = uncutData.shape
-
-    limit_rows = (1 - threshold) * data_size[1]
-    limit_cols = (1 - threshold) * data_size[0]
-
-    cut_row_count = 0
-    cut_col_count = 0
-
-    # cut along genes
-
-    for row_name in range(data_size[0]):
-        count = 0
-        for col_name in range(data_size[1]):
-            if np.isnan(uncutData[row_name, col_name]):
-                count += 1
-        if count >= limit_rows:
-            #print(cutData.shape, rows[row_name] )
-            cutData = np.delete(cutData, cut_row_count, 0)
-            cut_row_count -= 1
-        cut_row_count += 1
-
-    # cut along cell lines
-    data_size = cutData.shape
-    freshlyChopped = cutData
-
-    for col_name in range(data_size[1]):
-        count = 0
-        for row_name in range(data_size[0]):
-            if np.isnan(cutData[row_name, col_name]):
-                count += 1
-        if count >= limit_cols:
-            freshlyChopped = np.delete(freshlyChopped, cut_col_count, 1)
-            cut_col_count -= 1
-        cut_col_count += 1
-
-    return (freshlyChopped)
+def DataWorkFlow (username, password, threshold):
+    
+    #import data from synapse
+    syn = Synapse()
+    syn.login(username, password)
+    meth0 = pd.read_csv(syn.get('syn21533071').path, index_col = 0)
+    copy0 = pd.read_csv(syn.get('syn21533091').path, index_col = 0)
+    gene0 = pd.read_csv(syn.get('syn21533090').path, index_col = 0)
+    
+    #run filter - aligns all 3 data sets to the same size
+    meth1, gene1, copy1 = filterData(meth0, gene0, copy0)
+    
+    #cut missing values from methylation data
+    meth1_5 = cutMissingValues(meth1, threshold)
+    
+    #redo filter to make the data sets the same size again
+    meth2, gene2, copy2 = filterData(meth1_5, gene1, copy1)
+    
+    return meth2, gene2, copy2
 
 
 def normalize(data):

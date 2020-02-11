2706055025eb045180e73687187201627c8ecd0b
Nilay Shah <nilay.shah19@gmail.com>
2020-02-10 18:26:40 -0800

    Error Metrics Function and Decision Tree Model (#65)
    * Decision Tree Model
    
    * Updated Metrics function
    
    Co-authored-by: Micah Bryant <39173191+micah-bryant@users.noreply.github.com>
    
    Former-commit-id: 5f20b2b5de8b3b566807e6db12b93b94326db58c

--- tfac/regression.py
+++ tfac/regression.py
@@ -2,23 +2,54 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error, r2_score
 import xgboost as xgb
 from xgboost import XGBRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error r2_score
 from sklearn.ensemble import RandomForestRegressor
 
-def errMetric(y_test, y_pred):
+sns.set()
+
+def errorMetrics(y_test, y_pred):
     '''
     Determines error values based off of predicted and actual values
     Inputs: 1D Numpy Arrays
     Outputs: Prints rmse and r2  and returns them as Float64
     '''
+    weightedRes = (y_pred - y_test)/ y_test
+    absError = abs(weightedRes) * 100
+    sqError = (weightedRes**2) * 100
+    
+    mape = np.round(np.mean(absError), 2)
+    mspe = np.round(np.mean(sqError), 2)
     rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    mae = mean_absolute_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
+    
     print('Model Performance')
     print('Root Mean Squared Error: {:0.4f}'.format(rmse))
-    print('R2 Score = {:0.4f}'.format(r2))
-    return rmse, r2
+    print('Mean Absolute Error: {:0.4f}'.format(mae))
+    print('Accuracy (MSPE): {:0.2f}'.format(100 - mspe))
+    print('Accuracy (MAPE): {:0.2f}'.format(100 - mape))
+    print('R2 Score: {:0.4f}'.format(r2))
+    metrics = np.array([rmse, mae, mspe, mape, r2])
+    return metrics
+
+# Decision Tree Model
+def dTreePred(xTrain, yTrain, xTest, yTest):
+    '''
+    Makes a prediction after fitting the model to the training data
+
+    Inputs: 2D Numpy Array, 1D Numpy Array, 2D Numpy Array, 1D Numpy Array
+
+    Outputs: 1D Numpy Array, 1D Numpy Array
+    '''
+
+    dTree = DecisionTreeRegressor(random_state = 42)
+    dTree.fit(X_train, y_train)
+    yPred = dTree.predict(xTest)
+    metrics = errorMetrics(yTest, yPred)
+    return yPred, metrics
 
 # XGBoost Model
 def xgbPred(xTrain, yTrain, xTest, yTest):
@@ -27,15 +58,15 @@ def xgbPred(xTrain, yTrain, xTest, yTest):
 
     Inputs: 2D Numpy Array, 1D Numpy Array, 2D Numpy Array, 1D Numpy Array
 
-    Outputs: 1D Numpy Array, float64, float64
+    Outputs: 1D Numpy Array, 1D Numpy Array
     '''
 
     xgb_model = XGBRegressor(objective='reg:squarederror', n_estimators=1000, seed=24,
                              subsample=0.1, colsample_bytree=0.1)
     xgb_model.fit(X_train, y_train)
     yPred = xgb_model.predict(xTest)
-    rmse, r2 = errMetric(yTest, yPred)
-    return yPred, rmse, r2
+    metrics = errorMetrics(yTest, yPred)
+    return yPred, metrics
 
 def xgbPlot(xTrain, yTrain, xTest, yTest, title, tree=False):#could potentially generalize to all regressions
     '''
@@ -69,11 +100,11 @@ def rfPred(xTrain, yTrain, xTest, yTest):
 
     Inputs: 2D Numpy Array, 1D Numpy Array, 2D Numpy Array, 1D Numpy Array
 
-    Outputs: 1D Numpy Array, float64, float64
+    Outputs: 1D Numpy Array, 1D Numpy Array
     '''
 
     rf = RandomForestRegressor(random_state = 42, n_estimators = 2000)
     rf.fit(X_train, y_train)
     yPred = rf.predict(xTest)
-    rmse, r2 = errMetric(yTest, yPred)
-    return yPred, rmse, r2
+    metrics = errorMetrics(yTest, yPred)
+    return yPred, metrics

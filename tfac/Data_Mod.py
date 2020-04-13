9880af41761279018d3a0862aacd2fa70fc0c2e4
scottdtaylor95 <48601443+scottdtaylor95@users.noreply.github.com>
2020-04-13 13:54:15 -0700

    Tensor stack (#104)
    * Make tensor stacking function (previous didn't track)
    
    * change DataHelpers import
    
    * delete import
    
    * Make tensor stacking function (previous didn't track)
    
    * change DataHelpers import
    
    * delete import
    
    * Moved notebook
    
    * update branch (#103)
    
    * Change PBS treatment time (#99)
    
    * Update File A
    
    * Update File B
    
    * Update File C
    
    * Adding Data modification function and removed normalization import th… (#100)
    
    * Adding Data modification function and removed normalization import that was causing bugs
    
    * Quick Fixes
    
    Co-authored-by: Robby <robcatdude@yahoo.com>
    
    Co-authored-by: aditya-sivakumar <50721969+aditya-sivakumar@users.noreply.github.com>
    Co-authored-by: Robby <robcatdude@yahoo.com>
    
    * Put functions in Data_Mod
    
    * Stack Function
    
    Co-authored-by: aditya-sivakumar <50721969+aditya-sivakumar@users.noreply.github.com>
    Co-authored-by: Robby <robcatdude@yahoo.com>
    Co-authored-by: robthemike1 <43423961+robthemike1@users.noreply.github.com>
    Former-commit-id: abac74c01694eb3c50e34b5f8a2f9ee83286364d

--- tfac/Data_Mod.py
+++ tfac/Data_Mod.py
@@ -2,11 +2,27 @@ import pandas as pd
 import numpy as np
 from .dataHelpers import importLINCSprotein
 
-
-def data_mod(x):
+def data_mod(x, df = None):
     '''Creates a slice of the data tensor corresponding to the inputted treatment'''
-    df = importLINCSprotein()
+    if type(df) != pd.core.frame.DataFrame:
+        df = importLINCSprotein()
     spec_df = df.loc[(df['Treatment'] == 'Control') | (df['Treatment'] == x)]
-    spec_df = spec_df.drop(columns=['Sample description', 'Treatment', 'File'])
+    times = spec_df['Time'].to_numpy().tolist()
+    spec_df = spec_df.drop(columns = ['Sample description', 'Treatment', 'File', 'Time'])
     y = spec_df.to_numpy()
-    return y, spec_df
+    return y, spec_df, times
+
+def form_tensor():
+    '''Creates tensor in numpy array form and returns tensor, treatments, and time'''
+    df = importLINCSprotein()
+
+    unique_treatments = np.unique(df['Treatment'].values).tolist()
+    unique_treatments.remove('Control')
+
+    slices = []
+    df_slices = []
+    for treatment in unique_treatments:
+        array, df_array, times = data_mod(treatment, df)
+        slices.append(array)
+    tensor = np.stack(slices)
+    return tensor, unique_treatments, times

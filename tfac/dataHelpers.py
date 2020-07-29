dc131c4318e64fb4d8fedc97239aa912953919a8
Aaron Meyer <git@ameyer.me>
2020-07-29 10:46:47 -0700

    Remove OHSU files.
    Former-commit-id: 926c9f2642c6c73775c02af941d6f64577bc157b

--- tfac/dataHelpers.py
+++ tfac/dataHelpers.py
@@ -1,34 +1,9 @@
 """Contains function for importing and handling OHSU data"""
-from os.path import join, dirname
+from os.path import dirname
 import numpy as np
 import pandas as pd
 
 path_here = dirname(dirname(__file__))
-
-
-def importLINCSprotein():
-    """ Import protein characterization from LINCS. """
-    dataA = pd.read_csv(join(path_here, "tfac/data/01_Laura_Heiser__Sean_Gross_A.csv"))
-    dataB = pd.read_csv(join(path_here, "tfac/data/01_Laura_Heiser__Sean_Gross_B.csv"))
-    dataC = pd.read_csv(join(path_here, "tfac/data/01_Laura_Heiser__Sean_Gross_C.csv"))
-
-    dataA["File"] = "A"
-    dataB["File"] = "B"
-    dataC["File"] = "C"
-
-    return pd.concat([dataA, dataB, dataC])
-
-
-def ohsu_data():
-    """ Import OHSU data for PARAFAC2"""
-    atac = pd.read_csv(join(path_here, "tfac/data/ohsu/MDD_ATACseq_Level4.csv"))
-    cycIF = pd.read_csv(join(path_here, "tfac/data/ohsu/MDD_cycIF_Level4.csv"))
-    GCP = pd.read_csv(join(path_here, "tfac/data/ohsu/MDD_GCP_Level4.csv"))
-    IF = pd.read_csv(join(path_here, "tfac/data/ohsu/MDD_IF_Level4.csv"))
-    L1000 = pd.read_csv(join(path_here, "tfac/data/ohsu/MDD_L1000_Level4.csv"))
-    RNAseq = pd.read_csv(join(path_here, "tfac/data/ohsu/MDD_RNAseq_Level4.csv"))
-    RPPA = pd.read_csv(join(path_here, "tfac/data/ohsu/MDD_RPPA_Level4.csv"))
-    return atac, cycIF, GCP, IF, L1000, RNAseq, RPPA
 
 
 def compProteins(comps):

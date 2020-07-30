df2d7aa20cd66e1ba950888932a1bf2bdba24c32
Aaron Meyer <git@ameyer.me>
2020-07-30 11:10:29 -0700

    Remove more.

--- tfac/tensor.py
+++ tfac/tensor.py
@@ -2,24 +2,10 @@
 Tensor decomposition methods
 """
 import numpy as np
-import tensorly as tl
 from tensorly.decomposition import parafac2
 from tensorly.metrics.regression import variance as tl_var
 from tensorly.parafac2_tensor import parafac2_to_slice
 
-
-tl.set_backend("numpy")  # Set the backend
-
-
-def z_score_values(A, cell_dim):
-    """ Function that takes in the values tensor and z-scores it. """
-    assert cell_dim < tl.ndim(A)
-    convAxes = tuple([i for i in range(tl.ndim(A)) if i != cell_dim])
-    convIDX = [None] * tl.ndim(A)
-    convIDX[cell_dim] = slice(None)
-
-    sigma = tl.tensor(np.std(tl.to_numpy(A), axis=convAxes))
-    return A / sigma[tuple(convIDX)]
 
 def R2Xparafac2(tensor_slices, decomposition):
     """Calculate the R2X of parafac2 decomposition"""

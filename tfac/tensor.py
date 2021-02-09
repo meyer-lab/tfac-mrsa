44940e2ebe22fd73a2265a3b67dcb22bc57b6585
Aaron Meyer <ameyer@ucla.edu>
2021-02-09 12:57:52 -0800

    Add base TMTF methods
    Former-commit-id: c2f9c613fb4a6d2016f3572a3f18c68b28ecaabd

--- tfac/tensor.py
+++ tfac/tensor.py
@@ -2,6 +2,7 @@
 Tensor decomposition methods
 """
 import numpy as np
+import tensorly as tl
 from tensorly.decomposition import parafac2
 from tensorly.parafac2_tensor import parafac2_to_slice
 
@@ -15,6 +16,13 @@ def R2Xparafac2(tensor_slices, decomposition):
     return R2XX
 
 
+def calcR2X(tensorIn, matrixIn, tensorFac, matrixFac):
+    """ Calculate R2X. """
+    tErr = np.nanvar(tl.cp_to_tensor(tensorFac) - tensorIn)
+    mErr = np.nanvar(tl.cp_to_tensor(matrixFac) - matrixIn)
+    return 1.0 - (tErr + mErr) / (np.nanvar(tensorIn) + np.nanvar(matrixIn))
+
+
 #### Decomposition Methods ###################################################################
 
 
@@ -26,3 +34,36 @@ def MRSA_decomposition(tensor_slices, components, **kwargs):
         tensor_slices list
     """
     return parafac2(tensor_slices, components, **kwargs)
+
+
+def reorient_factors(tensorFac, matrixFac):
+    """ This function ensures that factors are negative on at most one direction. """
+    for jj in range(1, len(tensorFac)):
+        # Calculate the sign of the current factor in each component
+        means = np.sign(np.mean(tensorFac[jj], axis=0))
+
+        # Update both the current and last factor
+        tensorFac[0] *= means[np.newaxis, :]
+        matrixFac[0] *= means[np.newaxis, :]
+        matrixFac[1] *= means[np.newaxis, :]
+        tensorFac[jj] *= means[np.newaxis, :]
+    return tensorFac, matrixFac
+
+
+def censored_lstsq(A, B):
+    """Solves least squares problem subject to missing data.
+    Note: uses a for loop over the columns of B, leading to a
+    slower but more numerically stable algorithm
+    Args
+    ----
+    A (ndarray) : m x r matrix
+    B (ndarray) : m x n matrix
+    Returns
+    -------
+    X (ndarray) : r x n matrix that minimizes norm(M*(AX - B))
+    """
+    X = np.empty((A.shape[1], B.shape[1]))
+    for i in range(B.shape[1]):
+        m = np.isfinite(B[:, i])  # drop rows where mask is zero
+        X[:, i] = np.linalg.lstsq(A[m], B[m, i], rcond=None)[0]
+    return X.T

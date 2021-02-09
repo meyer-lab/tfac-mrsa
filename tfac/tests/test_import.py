e763f31ec7822cb5ad2bb8a31830fd090770794f
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2021-02-09 08:18:06 -0800

    Test (#23)
    * More testing
    
    * More tests

--- tfac/tests/test_import.py
+++ tfac/tests/test_import.py
@@ -4,7 +4,7 @@ Test that we can successfully import the datasets.
 import pytest
 import numpy as np
 import pandas as pd
-from ..dataImport import importCohort1Expression, importCohort3Expression, full_import, form_missing_tensor, get_C1_patient_info
+from ..dataImport import importCohort1Expression, importCohort3Expression, full_import, form_missing_tensor, get_C1_patient_info, form_MRSA_tensor
 
 
 @pytest.mark.parametrize("call", [importCohort1Expression, importCohort3Expression, get_C1_patient_info])
@@ -19,6 +19,7 @@ def test_fullImport():
     """ Test that we can import the full dataset. """
     cyto_list, cytokines, dfExp, geneIDs = full_import()
 
+    assert isinstance(geneIDs, list)
     assert isinstance(dfExp, pd.DataFrame)
     assert isinstance(cyto_list, list)
 
@@ -33,5 +34,18 @@ def test_formMissing():
         assert isinstance(tensor_slices[i], np.ndarray)
         assert np.any(np.isnan(tensor_slices[i]))
 
+    assert isinstance(geneIDs, list)
     assert tensor_slices[0].shape[1] == tensor_slices[1].shape[1]
     assert tensor_slices[0].shape[1] == tensor_slices[2].shape[1]
+
+
+@pytest.mark.parametrize("dataType", ["serum", "plasma"])
+def test_formTensor(dataType):
+    """ Test that the most basic imports work. """
+    tensor_slices, cytokines, geneIDs, cohortID = form_MRSA_tensor(dataType)
+
+    assert len(tensor_slices) == 2
+    assert np.all(np.isfinite(tensor_slices[1])) # Gene expression should be complete
+    assert isinstance(cohortID, list)
+    assert isinstance(geneIDs, list)
+    assert tensor_slices[0].shape[1] == tensor_slices[1].shape[1]

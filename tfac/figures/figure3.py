5da5c62ca0bdcc02914656807a24f405a0ba9b04
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2021-02-09 10:35:19 -0800

    Test and linting (#25)
    * More testing
    
    * More tests
    
    * Fix
    
    * Linting
    
    Former-commit-id: e0b50ca3f7d550a6dd49bd7a412733ea2c3780e6

--- tfac/figures/figure3.py
+++ tfac/figures/figure3.py
@@ -28,10 +28,14 @@ def fig_3_setup():
     cyto_df = cyto_df.drop([25, 43, 46])
 
     Cytotoxic_T = (deconv["T cell CD8+"].values - deconv["T cell CD8+"].mean()) / deconv["T cell CD8+"].std()
-    T_mem_active = (deconv["T cell CD4+ memory activated"].values - deconv["T cell CD4+ memory activated"].mean()) / deconv["T cell CD4+ memory activated"].std()
+    T_mem_active = (deconv["T cell CD4+ memory activated"].values - deconv["T cell CD4+ memory activated"].mean()) / deconv[
+        "T cell CD4+ memory activated"
+    ].std()
     T_regs = (deconv["T cell regulatory (Tregs)"].values - deconv["T cell regulatory (Tregs)"].mean()) / deconv["T cell regulatory (Tregs)"].std()
     T_naive = (deconv["T cell CD4+ naive"].values - deconv["T cell CD4+ naive"].mean()) / deconv["T cell CD4+ naive"].std()
-    T_mem_resting = (deconv["T cell CD4+ memory resting"].values - deconv["T cell CD4+ memory resting"].mean()) / deconv["T cell CD4+ memory resting"].std()
+    T_mem_resting = (deconv["T cell CD4+ memory resting"].values - deconv["T cell CD4+ memory resting"].mean()) / deconv[
+        "T cell CD4+ memory resting"
+    ].std()
     T_follicular = (deconv["T cell follicular helper"].values - deconv["T cell follicular helper"].mean()) / deconv["T cell follicular helper"].std()
     Mast_active = (deconv["Mast cell activated"].values - deconv["Mast cell activated"].mean()) / deconv["Mast cell activated"].std()
     Mac_0 = (deconv["Macrophage M0"].values - deconv["Macrophage M0"].mean()) / deconv["Macrophage M0"].std()
@@ -47,16 +51,16 @@ def makeFigure():
     cell_df = fig_3_setup()
     # Get list of axis objects
     ax, f = getSetup((18, 9), (1, 2))
-    b = sns.scatterplot(data=cell_df, x='Component A', y='A', hue="Outcomes", ax=ax[0], s=70)
+    b = sns.scatterplot(data=cell_df, x="Component A", y="A", hue="Outcomes", ax=ax[0], s=70)
     b.set_xlabel("Component A", fontsize=25)
     b.set_ylabel("Active T Cells minus T regs/Inactive T cells", fontsize=25)
     b.tick_params(labelsize=20)
-    ax[0].legend(fontsize=25, loc='upper left')
-    b = sns.scatterplot(data=cell_df, x='Component B', y='B', hue="Outcomes", ax=ax[1], s=70)
+    ax[0].legend(fontsize=25, loc="upper left")
+    b = sns.scatterplot(data=cell_df, x="Component B", y="B", hue="Outcomes", ax=ax[1], s=70)
     b.set_xlabel("Component B", fontsize=25)
     b.set_ylabel("Active Mast Cells, M0, and M1 Macrophages", fontsize=25)
     b.tick_params(labelsize=20)
-    ax[1].legend(fontsize=25, loc='upper left')
+    ax[1].legend(fontsize=25, loc="upper left")
     # Add subplot labels
     subplotLabel(ax)
 

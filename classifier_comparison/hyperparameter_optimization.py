2dbc60f7db8cb86865bd26e3be0644b560c77ec5
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2021-04-26 07:22:13 -0700

    Hyperparameter Fitting (#52)
    * Fixed bug in CV where AUC-ROC of last fold was returned instead of
    average AUC-ROC
    
    * Added data normalization option to training.
    
    * Changed sequential feature selection to run backwards.
    
    * Added hyperparameter optimization and exhaustive feature selection.
     - Added hyperparameter_optimization.py to identify optimal classifier
     hyperparameters
     - Added exhaustive feature selection option to
     classifier_feat_comparison.py
    
    * Updated environment .yml file

---
+++ classifier_comparison/hyperparameter_optimization.py
@@ -0,0 +1,184 @@
+"""
+Runs hyperparameter optimization for each of the classifiers specified in the
+CLASSIFIERS dict below. This code can be called via the command line as 
+follows:
+
+python hyperparamter_optimization.py -d [path/to/data.pkl] -l [path/to/label.pkl] -m [number of evals]
+"""
+import argparse
+from functools import partial
+import pickle
+import os
+import sys
+import warnings
+
+from hyperopt import fmin, hp, tpe, Trials
+from hyperopt.pyll import scope
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.feature_selection import SelectKBest, SequentialFeatureSelector
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+from classifier_feat_comparison import run_sequential, run_exhaustive
+from dataImport import form_missing_tensor
+
+warnings.filterwarnings('ignore', category=ConvergenceWarning)
+
+CLASSIFIERS = {
+    'Logistic Regression': {
+        'model': LogisticRegression, 
+        'file': 'lr_hp_trials.pkl',
+        'space': {
+            'C': hp.normal('C', 1, 0.5),
+            'n_feats': scope.int(hp.quniform('n_feats', 1, 39, 1))
+        },
+    },
+    'SVC_rbf': {
+        'model': SVC, 
+        'file': 'SVC_rbf_hp_trials.pkl',
+        'space': {
+            'C': hp.normal('C', 1, 0.5),
+            'gamma': hp.lognormal('gamma', 0.5, 0.25),
+            'kernel': 'rbf',
+            'n_feats': scope.int(hp.quniform('n_feats', 1, 39, 1)),
+            'probability': True
+        },
+    },
+    'Gaussian Naive Bayes': {
+        'model': GaussianNB, 
+        'file': 'gnb_hp_trials.pkl',
+        'space': {
+            'var_smoothing': hp.loguniform('var_smoothing', 1E-8, 1E-1),
+            'n_feats': scope.int(hp.quniform('n_feats', 1, 39, 1))
+        },
+    },
+}
+
+
+def obj_func(space, clf_name, data, labels, exhaustive):
+    """
+    Defines objective function for Hyperopt to minimize.
+
+    Parameters:
+        space (dict): dict mapping hyperparameter names to search spaces
+        clf_name (str): name of classifier
+        data (pandas.DataFrame): data for classification
+        labels (pandas.Series): labels for data samples
+        exhaustive (bool): whether to run exhaustive feature selection
+
+    Returns:
+        Average loss across folds, given as (1 - average accuracy)
+    """
+    n_feats = space.pop('n_feats')
+
+    for key in space.keys():
+        if isinstance(space[key], float):
+            space[key] = max(1E-8, space[key])
+
+        if isinstance(space[key], int):
+            space[key] = max(0, space[key])
+
+    clf = CLASSIFIERS[clf_name]['model'](**space)
+
+    if exhaustive:
+        auc_score, _ = run_exhaustive(clf, data, labels, n_splits=30)
+    else:
+        auc_score, _ = run_sequential(clf, data, labels, n_feats, n_splits=30)
+
+    return 1 - auc_score
+
+
+def main(parser):
+    # Reads parser arguments
+    data = pd.read_pickle(parser.data)
+    labels = pd.read_pickle(parser.labels)
+    exhaustive = parser.exhaustive
+    max_evals = parser.max_evals
+
+    # Reduces data to only 40-factor decomposition
+    data = data[-1][0].factors[0]
+    data = pd.DataFrame(data)
+
+    # Casts labels to int and removes samples with unknown outcomes
+    labels = labels.reset_index(drop=True)
+    labels = labels.loc[labels != 'Unknown']
+    labels = labels.astype(int)
+    data = data.loc[labels.index, :]
+
+    # Runs for each classifier
+    for clf_name in CLASSIFIERS.keys():
+        file_name = os.path.join(os.getcwd(), CLASSIFIERS[clf_name]['file'])
+        if exhaustive:
+            names = file_name.split('.')
+            file_name = names[0] + '_exhaustive.' + names[1]
+
+        # Check if trials object already exists--if not, makes a new one
+        try:
+            trials = pickle.load(open(file_name, 'rb'))
+        except FileNotFoundError:
+            trials = Trials()
+
+        # Declares objective function to minimize--partial used to pass constant arguments
+        # to objective function
+        fmin_objective = partial(obj_func, clf_name=clf_name, data=data, labels=labels, exhaustive=exhaustive)
+        opt_params = fmin(fn=fmin_objective, algo=tpe.suggest, space=CLASSIFIERS[clf_name]['space'],
+                          max_evals=max_evals, trials=trials)
+
+        # Dumps trial object to file
+        with open(file_name, 'wb') as handle:
+            pickle.dump(trials, handle, protocol=pickle.HIGHEST_PROTOCOL)        
+
+
+def _read_args():
+    """
+    Reads command line arguments--we use these to specify pickle files for
+    classification
+
+    Parameters:
+        None
+
+    Returns:
+        argparse.ArgumentParser with command line arguments
+    """
+    parser = argparse.ArgumentParser(
+        description='Specify files for classification hyper-parameter optimization'
+    )
+    parser.add_argument(
+        '-d',
+        '--data',
+        dest='data',
+        type=str,
+        help='Data pickle file path',
+    )
+    parser.add_argument(
+        '-e',
+        '--exhaustive', 
+        dest='exhaustive',
+        action='store_true',
+        help='Enables exhaustive feature search',
+    )
+    parser.add_argument(
+        '-l',
+        '--labels', 
+        dest='labels',
+        type=str, 
+        help='Label pickle file path',
+    )
+    parser.add_argument(
+        '-m',
+        '--max_evals', 
+        dest='max_evals',
+        type=int, 
+        help='Maximum hyperopt evals per classifier',
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    parser = _read_args()
+    main(parser)

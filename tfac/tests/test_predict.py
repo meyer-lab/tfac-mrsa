56432b801de83b28fd1902ac1d0f651121ab4ce0
Aaron Meyer <ameyer@ucla.edu>
2021-05-06 08:57:13 -0700

    Initial fixes

--- tfac/tests/test_predict.py
+++ tfac/tests/test_predict.py
@@ -5,7 +5,7 @@ from sklearn.datasets import load_breast_cancer
 from ..predict import SVC_predict
 
 
-def test_TMTF():
+def test_CMTF():
     """ Test that the nested CV function is working. """
     data = load_breast_cancer()
 

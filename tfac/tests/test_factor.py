524a2be8adda4751fdbfdbf097807808522863ee
Aaron Meyer <ameyer@ucla.edu>
2021-08-04 13:50:00 -0700

    Some cleanup

--- tfac/tests/test_factor.py
+++ tfac/tests/test_factor.py
@@ -2,16 +2,13 @@
 Test that we can factor the data.
 """
 import numpy as np
-from ..dataImport import form_missing_tensor
+from ..dataImport import get_scaled_tensors
 from ..tensor import perform_CMTF
 
 
 def test_CMTF():
     """ Test that we can form the missing tensor. """
-    tensor_slices, _, _, _ = form_missing_tensor()
-
-    tensor = np.stack((tensor_slices[0], tensor_slices[1])).T
-    matrix = tensor_slices[2].T
+    tensor, matrix, _ = get_scaled_tensors()
 
     tFac = perform_CMTF(tensor, matrix, r=3)
 

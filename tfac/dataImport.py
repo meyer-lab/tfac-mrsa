6e2a70644166a6e299c224b448d851c669b21b65
Jackson Chin <JacksonLymanChin@gmail.com>
2021-08-09 17:50:57 -0700

    Merged data files and updated imports  - Merged all like data files across cohorts into single pickle  files  - Updated data imports; refactored data preprocessing to improve  organization  - Added form_tensor to form tensor and matrix for CMTF

--- tfac/dataImport.py
+++ tfac/dataImport.py
@@ -35,13 +35,182 @@ def get_scaled_tensors(scaling: float = 1.0):
     return tensor, matrix, labels
 
 
-def import_deconv():
-    """ Imports and returns cell deconvolution data. """
-    return (
-        pd.read_csv(join(PATH_HERE, "tfac/data/mrsa/deconvo_cibersort_APMB.csv"), delimiter=",", index_col="sample")
-        .sort_index()
-        .drop(["gender", "cell_type"], axis=1)
+# NEW IMPORTS
+
+
+def import_patient_metadata():
+    """
+    Returns patient meta data, including cohort and outcome.
+
+    Parameters:
+        None
+
+    Returns:
+        patient_data (pandas.DataFrame): Patient outcomes and cohorts
+    """
+    patient_data = pd.read_pickle(
+        join(PATH_HERE, 'tfac', 'data', 'mrsa', 'patient_metadata.pkl')
     )
+    return patient_data
+
+
+def import_cytokines(scale_cyto=True):
+    """
+    Return plasma and serum cytokine data.
+
+    Parameters:
+        scale_cyto (bool, default:True): scale cytokine values
+
+    Returns:
+        plasma_cyto (pandas.DataFrame): plasma cytokine data
+        serum_cyto (pandas.DataFrame): serum cytokine data
+    """
+    plasma_cyto = pd.read_pickle(
+        join(PATH_HERE, 'tfac', 'data', 'mrsa', 'plasma_cytokines.pkl')
+    )
+    serum_cyto = pd.read_pickle(
+        join(PATH_HERE, 'tfac', 'data', 'mrsa', 'serum_cytokines.pkl')
+    )
+    patient_data = import_patient_metadata()
+
+    cohort_1 = patient_data.loc[patient_data['cohort'] == 1].index
+    c1_plasma = set(cohort_1) & set(plasma_cyto.index)
+    c1_serum = set(cohort_1) & set(serum_cyto.index)
+
+    plasma_p70 = [val * 123000000 if val < 1 else val for val in plasma_cyto.loc[c1_plasma, 'IL-12(p70)']]
+    serum_p70 = [val * 123000000 if val < 1 else val for val in serum_cyto.loc[c1_serum, 'IL-12(p70)']]
+    plasma_cyto.loc[c1_plasma, 'IL-12(p70)'] = plasma_p70
+    serum_cyto.loc[c1_serum, 'IL-12(p70)'] = serum_p70
+
+    if scale_cyto:
+        plasma_cyto = scale_cytokines(plasma_cyto)
+        serum_cyto = scale_cytokines(serum_cyto)
+
+    return plasma_cyto.T, serum_cyto.T
+
+
+def scale_cytokines(cyto):
+    """
+    Scales provided cytokine data--performs a log-transform, then
+    zero-mean centers.
+
+    Parameters:
+        cyto (pandas.DataFrame): cytokine data
+
+    Returns:
+        cyto (pandas.DataFrame): scaled cytokine data
+    """
+    cyto = cyto.transform(np.log)
+    cyto -= cyto.mean(axis=0)
+    cyto = cyto.sub(cyto.mean(axis=1), axis=0)
+
+    return cyto
+
+
+def import_rna(trim_low=True, scale_rna=True):
+    """
+    Return RNA expression data.
+
+    Parameters:
+        trim_low (bool, default:True): remove genes with low expression counts
+        scale_rna (bool, default:True): zero-mean scale RNA expression
+
+    Returns:
+        rna (pandas.DataFrame): RNA expression counts
+    """
+    rna = pd.read_pickle(
+        join(PATH_HERE, 'tfac', 'data', 'mrsa', 'rna_expression.pkl')
+    )
+
+    if trim_low:
+        high_means = rna.mean(axis=0) > 1.0
+        rna = rna.loc[:, high_means]
+
+    if scale_rna:
+        columns = rna.columns
+        rna = rna.apply(scale, axis=1, result_type='expand')
+        rna.columns = columns
+        rna = rna.apply(scale, axis=0)
+
+    return rna.T
+
+
+def form_tensor(variance_scaling: float = 1.0):
+    """
+    Forms a tensor of cytokine data and a matrix of RNA expression data for
+    CMTF decomposition.
+
+    Parameters:
+        variance_scaling (float, default:1.0): RNA/cytokine variance scaling
+
+    Returns:
+        tensor (numpy.array): tensor of cytokine data
+        matrix (numpy.array): matrix of RNA expression data
+    """
+    plasma_cyto, serum_cyto = import_cytokines()
+    rna = import_rna()
+    patient_data = import_patient_metadata()
+
+    patients = set(patient_data.index)
+    missing_serum = patients.difference(serum_cyto.columns)
+    missing_plasma = patients.difference(plasma_cyto.columns)
+    missing_rna = patients.difference(rna.columns)
+
+    serum_cyto = pd.concat(
+        [
+            serum_cyto,
+            pd.DataFrame(
+                data=np.nan,
+                index=serum_cyto.index,
+                columns=missing_serum
+            )
+        ],
+        axis=1
+    )
+    plasma_cyto = pd.concat(
+        [
+            plasma_cyto,
+            pd.DataFrame(
+                data=np.nan,
+                index=plasma_cyto.index,
+                columns=missing_plasma
+            )
+        ],
+        axis=1
+    )
+    rna = pd.concat(
+        [
+            rna,
+            pd.DataFrame(
+                data=np.nan,
+                index=rna.index,
+                columns=missing_rna
+            )
+        ],
+        axis=1
+    )
+
+    serum_cyto = serum_cyto.sort_index(axis=1)
+    plasma_cyto = plasma_cyto.sort_index(axis=1)
+    rna = rna.sort_index(axis=1)
+
+    cyto_var = np.linalg.norm(serum_cyto.fillna(0)) + \
+               np.linalg.norm(plasma_cyto.fillna(0))
+    serum_cyto /= cyto_var
+    plasma_cyto /= cyto_var
+
+    rna_var = np.linalg.norm(rna.fillna(0))
+    rna /= rna_var * variance_scaling
+    matrix = rna.to_numpy(dtype=float)
+
+    tensor = np.stack(
+        (serum_cyto, plasma_cyto)
+    ).T
+
+    return tensor, matrix
+
+
+# OLD IMPORTS
 
 
 def get_C1C2_patient_info():
@@ -81,6 +250,8 @@ def form_missing_tensor(variance1: float = 1.0):
     patInfo = pd.concat([c1_info, c3_info], axis=1)
     temp = temp.append(patInfo).sort_values(by=["cohort", "type", "status"], axis=1)
     patInfo = temp.loc[["type", "status", "cohort"]]
+    temp = temp.sort_index(axis=1)
+
     # Assign data to matrices
     dfCyto_serum = temp.iloc[:38, :]
     dfCyto_plasma = temp.iloc[38:76, :]
@@ -136,6 +307,7 @@ def full_import():
     # Modify RNAseq
     dfExp_c1 = removeC1_dupes(dfExp_c1)
     # Drop genes not shared
+    # TODO: WE HERE
     dfExp_c1.sort_values("Geneid", inplace=True)
     dfExp_c3.sort_values("Geneid", inplace=True)
     dfExp = pd.concat([dfExp_c1, dfExp_c3], axis=1, join="inner")
@@ -147,10 +319,8 @@ def full_import():
     genes = dfExp.index
     # Normalize
 
-    dfExp[:] = scale(dfExp, axis=0)
-    dfExp[:] = scale(dfExp, axis=1)
-    # dfExp = scale(dfExp, axis=0)
-    # dfExp = scale(dfExp, axis=1)
+    dfExp = scale(dfExp, axis=0)
+    dfExp = scale(dfExp, axis=1)
     dfExp = pd.DataFrame(dfExp, index=genes, columns=pats)
     geneIDs = dfExp.index.to_list()
     return cyto_list, cytokines, dfExp, geneIDs

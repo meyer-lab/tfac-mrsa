8234479d1103bfe5410f03181adae032a677553b
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2021-08-12 16:15:51 -0700

    Update and rename MRSA_GO_analysis.Rmd to MRSA_GO_analysis.R

--- tfac/MRSA_GO_analysis.R
+++ tfac/MRSA_GO_analysis.R
@@ -1,25 +1,13 @@
----
-title: "Gene Ontology Analysis of Transcriptional Components"
-output: html_notebook
----
-
-```{r}
+# Gene Ontology Analysis of Transcriptional Components
 library(clusterProfiler)
 library(ggplot2)
-```
 
-```{r}
 data <- read.csv("tfac/data/mrsa/MRSA_GO_Input.csv")
 data <- data[, c(1, 2)]
 xx <- compareCluster(Gene~Components, data=data, fun='enrichGO', keyType="ENSEMBL", ont="BP",  OrgDb='org.Hs.eg.db', pAdjustMethod='hochberg')
 xxSimp <- simplify(xx, cutoff=0.7, by="p.adjust", select_fun=min)
-```
 
-```{r}
 ggplot(xxSimp, aes(x=Components, y=Description, colour=p.adjust)) + geom_point(aes(size=GeneRatio)) + theme_bw() + ggtitle("Biological Process") + theme(plot.title = element_text(hjust = 0.5))
-```
 
-```{r}
 OutputPath = '/Users/foo/Desktop/MRSA_GO_BP.png'
 ggsave(OutputPath, units='cm', width=20, height=15)
-```

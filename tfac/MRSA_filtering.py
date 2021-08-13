339eefd90a6613586b401a2fd68dbe59222c5098
Jackson Chin <JacksonLymanChin@gmail.com>
2021-08-12 19:12:43 -0700

    Updated MRSA filtering to import data via dataImport scripts

--- tfac/MRSA_filtering.py
+++ tfac/MRSA_filtering.py
@@ -1,10 +1,21 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
+
+from tfac.dataImport import form_tensor, import_rna
+from tfac.tensor import perform_CMTF
+
 
 def export_mrsa_inputfile(OutputPath):
     """Select top 10% genes per component and export csv file to run clusterProfiler in R"""
-    d = pd.read_csv('tfac/data/mrsa/cmtf_components_genes.csv')
-    d = pd.melt(frame=d, id_vars="Unnamed: 0", value_vars=d.columns[1:], var_name="Components", value_name="Weights")
+    tensor, matrix, _ = form_tensor(32)
+    rna = import_rna()
+
+    d = perform_CMTF(tensor, matrix, 9)
+    d = pd.DataFrame(d.mFactor)
+    d.columns = range(1, 10)
+    d.loc[:, 'ID'] = rna.index
+
+    d = pd.melt(frame=d, id_vars='ID', value_vars=d.columns[:-1], var_name="Components", value_name="Weights")
 
     dfs = []
     for ii in set(d["Components"]):

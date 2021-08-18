291bb14a1113a67149a41693c759fe362f240486
Jackson Chin <JacksonLymanChin@gmail.com>
2021-08-18 00:13:37 -0700

    Added figure5 and improved data handling  - Updated to only import data once between experiments  - Updated to match makeFigure style

--- tfac/figures/figure5.py
+++ tfac/figures/figure5.py
@@ -1,9 +1,9 @@
 from matplotlib.patches import Patch
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from tfac.figures.figureCommon import getSetup
 from tfac.dataImport import form_tensor, import_cytokines, import_rna
 from tfac.predict import predict_known, predict_unknown
 from tfac.tensor import perform_CMTF
@@ -12,14 +12,26 @@ OPTIMAL_SCALING = 32
 
 
 def makeFigure():
-    train_samples = run_cv()
-    validation_samples = run_unknown()
-    plot_results(train_samples, validation_samples)
+    data_types, patient_data = get_data_types()
+    train_samples = run_cv(data_types, patient_data)
+    validation_samples = run_unknown(data_types, patient_data)
+    fig = plot_results(train_samples, validation_samples)
+
+    return fig
 
 
-def run_unknown():
+def get_data_types():
+    """
+    Creates data for classification.
+
+    Parameters:
+        None
+
+    Returns:
+        data_types (list[tuple]): data sources and their names
+        patient_data (pandas.DataFrame): patient metadata
+    """
     plasma_cyto, serum_cyto = import_cytokines()
-    rna = import_rna()
     tensor, matrix, patient_data = form_tensor(OPTIMAL_SCALING)
     patient_data = patient_data.loc[:, ['status', 'type']]
 
@@ -31,7 +43,6 @@ def run_unknown():
         ('Plasma IL-10', plasma_cyto.loc['IL-10', :]),
         ('Serum Cytokines', serum_cyto.T),
         ('Serum IL-10', serum_cyto.loc['IL-10', :]),
-        ('RNA', rna.T),
         ('CMTF', pd.DataFrame(
             components,
             index=patient_data.index,
@@ -40,46 +51,48 @@ def run_unknown():
         )
     ]
 
-    predicted = pd.DataFrame(
+    return data_types, patient_data
+
+
+def run_unknown(data_types, patient_data):
+    """
+    Predicts samples with unknown outcomes.
+
+    Parameters:
+        data_types (list[tuple]): data sources to predict
+        patient_data (pandas.DataFrame): patient metadata
+
+    Returns:
+        predictions (pandas.Series): predictions for each data source
+    """
+    predictions = pd.DataFrame(
         index=patient_data.index
     )
-    predicted = predicted.loc[patient_data['status'] == 'Unknown']
+    predictions = predictions.loc[patient_data['status'] == 'Unknown']
 
     for data_type in data_types:
         source = data_type[0]
         data = data_type[1]
         labels = patient_data.loc[data.index, 'status']
 
-        predictions = predict_unknown(data, labels)
-        predicted.loc[predictions.index, source] = predictions
+        _predictions = predict_unknown(data, labels)
+        predictions.loc[_predictions.index, source] = _predictions
 
-    return predicted
+    return predictions
 
 
-def run_cv():
-    plasma_cyto, serum_cyto = import_cytokines()
-    rna = import_rna()
-    tensor, matrix, patient_data = form_tensor(OPTIMAL_SCALING)
-    patient_data = patient_data.loc[:, ['status', 'type']]
+def run_cv(data_types, patient_data):
+    """
+    Predicts samples with known outcomes via cross-validation.
 
-    components = perform_CMTF(tensor, matrix, 9)
-    components = components[1][0]
+    Parameters:
+        data_types (list[tuple]): data sources to predict
+        patient_data (pandas.DataFrame): patient metadata
 
-    data_types = [
-        ('Plasma Cytokines', plasma_cyto.T),
-        ('Plasma IL-10', plasma_cyto.loc['IL-10', :]),
-        ('Serum Cytokines', serum_cyto.T),
-        ('Serum IL-10', serum_cyto.loc['IL-10', :]),
-        ('RNA', rna.T),
-        ('CMTF', pd.DataFrame(
-            components,
-            index=patient_data.index,
-            columns=list(range(1, 10))
-        )
-        )
-    ]
-
-    predicted = pd.DataFrame(
+    Returns:
+        predictions (pandas.Series): predictions for each data source
+    """
+    predictions = pd.DataFrame(
         index=patient_data.index
     )
 
@@ -88,50 +101,71 @@ def run_cv():
         data = data_type[1]
         labels = patient_data.loc[data.index, 'status']
 
-        predictions = predict_known(data, labels)
-        predicted.loc[predictions.index, source] = predictions
+        _predictions = predict_known(data, labels)
+        predictions.loc[_predictions.index, source] = _predictions
 
-    predicted.loc[:, 'Actual'] = patient_data.loc[:, 'status']
+    predictions.loc[:, 'Actual'] = patient_data.loc[:, 'status']
 
-    return predicted
+    return predictions
 
 
 def plot_results(cv_results, val_results):
+    """
+    Plots predictions as heatmaps.
+
+    Parameters:
+        cv_results (pandas.DataFrame): predictions for samples with known
+            outcomes
+        val_results (pandas.DataFrame): predictions for samples with unknown
+            outcomes
+
+    Returns:
+        fig (matplotlib.Figure): figure depicting predictions for all samples
+    """
+    fig_size = (8, 8)
+    layout = (2, 1)
+    axs, fig = getSetup(
+        fig_size,
+        layout
+    )
+
+    # Cross-validation predictions
+
     cv_results = cv_results.loc[cv_results['Actual'] != 'Unknown']
     cv_results = cv_results.fillna(-1).astype(int)
+    axs[0] = sns.heatmap(
+        cv_results.T,
+        ax=axs[0],
+        cbar=False,
+        cmap=['dimgrey', '#ffd2d2', '#9caeff'],
+        vmin=-1
+    )
 
-    plt.figure(figsize=(10, 4))
-    sns.heatmap(cv_results.T, cbar=False, cmap=['dimgrey', '#ffd2d2', '#9caeff'], vmin=-1)
-
-    plt.xticks([], [])
-    plt.yticks(fontsize=12)
-    plt.xlabel('Patient', fontsize=12)
-
-    legend_elements = [Patch(facecolor='dimgrey', edgecolor='dimgrey', label='Data Not Available'),
-                       Patch(facecolor='#ffd2d2', edgecolor='#ffd2d2', label='Persistor'),
-                       Patch(facecolor='#9caeff', edgecolor='#9caeff', label='Resolver')]
-    plt.legend(handles=legend_elements, loc=[1.05, 0.4])
-
-    plt.subplots_adjust(left=0.2, right=0.75, top=0.9)
-    plt.show()
-
-    val_results = val_results.fillna(-1)
-
-    plt.figure(figsize=(10, 4))
-    sns.heatmap(val_results.T, cbar=False, cmap=['dimgrey', '#ffd2d2', '#9caeff'], vmin=-1, linewidths=1)
-
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
-    plt.xlabel('Patient', fontsize=12)
+    axs[0].set_xticks([])
+    axs[0].set_xlabel('Patient', fontsize=12)
 
     legend_elements = [Patch(facecolor='dimgrey', edgecolor='dimgrey', label='Data Not Available'),
                        Patch(facecolor='#ffd2d2', edgecolor='#ffd2d2', label='Persistor'),
                        Patch(facecolor='#9caeff', edgecolor='#9caeff', label='Resolver')]
-    plt.legend(handles=legend_elements, loc=[1.05, 0.4])
-    plt.subplots_adjust(left=0.2, right=0.75, bottom=0.25, top=0.9)
+    axs[0].legend(handles=legend_elements, loc=[1.05, 0.4])
 
-    plt.show()
+    # Validation set predictions
 
+    val_results = val_results.fillna(-1).astype(int)
+    sns.heatmap(
+        val_results.T,
+        ax=axs[1],
+        cbar=False,
+        cmap=['dimgrey', '#ffd2d2', '#9caeff'],
+        vmin=-1,
+        linewidths=1
+    )
 
-if __name__ == '__main__':
-    makeFigure()
+    axs[1].set_xlabel('Patient', fontsize=12)
+
+    legend_elements = [Patch(facecolor='dimgrey', edgecolor='dimgrey', label='Data Not Available'),
+                       Patch(facecolor='#ffd2d2', edgecolor='#ffd2d2', label='Persistor'),
+                       Patch(facecolor='#9caeff', edgecolor='#9caeff', label='Resolver')]
+    axs[1].legend(handles=legend_elements, loc=[1.05, 0.4])
+
+    return fig

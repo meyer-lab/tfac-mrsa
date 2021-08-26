76d903b1cfddcab90cd1aa83519719f15212881b
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2021-08-26 11:20:52 -0700

    SVD orthogonalize the subjects matrix (#102)
    * Try qr
    
    * Adjust tolerances
    
    * SVD
    
    Former-commit-id: c2fb1799854b3580c22c021ef98afcfb8b1584be

--- tfac/tensor.py
+++ tfac/tensor.py
@@ -166,11 +166,15 @@ def perform_CMTF(tOrig, mOrig, r=9):
 
             tFac.factors[m] = censored_lstsq(kr, unfolded[m].T, uniqueInfo[m])
 
-        if ii % 3 == 0:
+            if m == 0:
+                u, _, v = np.linalg.svd(tFac.factors[m], full_matrices=False)
+                tFac.factors[m] = u @ v
+
+        if ii % 10 == 0:
             R2X_last = tFac.R2X
             tFac.R2X = calcR2X(tFac, tOrig, mOrig)
 
-        if tFac.R2X - R2X_last < 1e-6:
+        if tFac.R2X - R2X_last < 1e-5:
             break
 
     tFac = cp_normalize(tFac)

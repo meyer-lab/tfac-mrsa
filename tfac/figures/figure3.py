12652506beaaa68e07e4c6b87328c5e25a16dea4
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2021-09-08 16:26:00 -0700

    Figure Reorganization (#118)
    * Updated figures 2 and 3 to match new formatting
     - Updated figure2 to show CMTF parameterization plots
     - Updated figure3 to show prediction model performance and
     validation results
     - TODO: Add race/sex/age model to figure3
    
    * Removed debugging tools from figure2
    
    * Updated documentation for figure scripts
    
    * Fixed limits on figure3 and added diagonal to ROC

--- tfac/figures/figure3.py
+++ tfac/figures/figure3.py
@@ -1,67 +1,307 @@
-"""
-This creates Figure 3 - Cytokine plots
-"""
+from matplotlib.patches import Patch
+import matplotlib.pyplot as plt
 import numpy as np
+from os.path import abspath, dirname, join
 import pandas as pd
 import seaborn as sns
-from scipy.stats import pearsonr
+from sklearn.metrics import roc_curve
+
+from tfac.figures.figureCommon import getSetup, OPTIMAL_SCALING
 from tfac.dataImport import form_tensor, import_cytokines
-from .figureCommon import getSetup
+from tfac.predict import predict_known, predict_unknown
+from tfac.tensor import perform_CMTF
+
+PATH_HERE = dirname(dirname(abspath(__file__)))
 
 
-def serum_vs_plasma_setup():
-    tensor, _, patInfo = form_tensor()
-    plasma, _ = import_cytokines()
-    cytokines = plasma.index
+def get_data_types():
+    """
+    Creates data for classification.
 
-    patInfo = patInfo.T
-    tensor = tensor.T
-    serum_slice = tensor[0, :, :]
-    plasma_slice = tensor[1, :, :]
+    Parameters:
+        None
 
-    test = pd.concat([pd.DataFrame(serum_slice), pd.DataFrame(plasma_slice)])
-    test = test.dropna(axis=1)
-    pears = []
-    for i in range(38):
-        pears.append([cytokines[i], pearsonr(test.iloc[i, :].to_numpy(dtype=float), test.iloc[i + 38, :].to_numpy(dtype=float))[0]])
-    pears = pd.DataFrame(pears).sort_values(1)
+    Returns:
+        data_types (list[tuple]): data sources and their names
+        patient_data (pandas.DataFrame): patient metadata
+    """
+    plasma_cyto, serum_cyto = import_cytokines()
+    tensor, matrix, patient_data = form_tensor(OPTIMAL_SCALING)
+    patient_data = patient_data.loc[:, ['status', 'type']]
 
-    ser = pd.DataFrame(serum_slice, index=cytokines, columns=patInfo.columns).T
-    ser["Outcome"] = patInfo.T["status"]
-    ser = ser[ser["Outcome"].isin(['0', '1'])].dropna().astype(float)
-    plas = pd.DataFrame(plasma_slice, index=cytokines, columns=patInfo.columns).T
-    plas["Outcome"] = patInfo.T["status"]
-    plas = plas[plas["Outcome"].isin(['0', '1'])].dropna().astype(float)
-    plas["Outcome"] += 2
-    cyto = pd.concat([ser, plas])
-    out0 = cyto[cyto["Outcome"] == 0]
-    out1 = cyto[cyto["Outcome"] == 1]
-    out2 = cyto[cyto["Outcome"] == 2]
-    out3 = cyto[cyto["Outcome"] == 3]
-    cyto = cyto.append(pd.DataFrame(np.abs(out0.median() - out1.median()) + np.abs(out2.median() - out3.median()), columns=["diff"]).T).sort_values(by="diff", axis=1).drop("diff")
-    cyto = pd.melt(cyto, id_vars=["Outcome"])
+    components = perform_CMTF(tensor, matrix)
+    components = components[1][0]
 
-    return pears, cyto
+    data_types = [
+        ('Plasma Cytokines', plasma_cyto.T),
+        ('Plasma IL-10', plasma_cyto.loc['IL-10', :]),
+        ('Serum Cytokines', serum_cyto.T),
+        ('Serum IL-10', serum_cyto.loc['IL-10', :]),
+        ('CMTF', pd.DataFrame(
+            components,
+            index=patient_data.index,
+            columns=list(range(1, components.shape[1] + 1))
+        )
+        )
+    ]
+
+    return data_types, patient_data
+
+
+def run_unknown(data_types, patient_data):
+    """
+    Predicts samples with unknown outcomes.
+
+    Parameters:
+        data_types (list[tuple]): data sources to predict
+        patient_data (pandas.DataFrame): patient metadata
+
+    Returns:
+        predictions (pandas.Series): predictions for each data source
+    """
+    predictions = pd.DataFrame(
+        index=patient_data.index
+    )
+    predictions = predictions.loc[patient_data['status'] == 'Unknown']
+
+    for data_type in data_types:
+        source = data_type[0]
+        data = data_type[1]
+        labels = patient_data.loc[data.index, 'status']
+
+        _predictions = predict_unknown(data, labels)
+        predictions.loc[_predictions.index, source] = _predictions
+
+    return predictions
+
+
+def run_cv(data_types, patient_data):
+    """
+    Predicts samples with known outcomes via cross-validation.
+
+    Parameters:
+        data_types (list[tuple]): data sources to predict
+        patient_data (pandas.DataFrame): patient metadata
+
+    Returns:
+        predictions (pandas.Series): predictions for each data source
+    """
+    predictions = pd.DataFrame(
+        index=patient_data.index
+    )
+    probabilities = predictions.copy()
+
+    for data_type in data_types:
+        source = data_type[0]
+        data = data_type[1]
+        labels = patient_data.loc[data.index, 'status']
+
+        _predictions = predict_known(data, labels)
+        _probabilities = predict_known(data, labels, method='predict_proba')
+
+        probabilities.loc[_probabilities.index, source] = _probabilities
+        predictions.loc[_predictions.index, source] = _predictions
+
+    predictions.loc[:, 'Actual'] = patient_data.loc[:, 'status']
+
+    return predictions, probabilities
+
+
+def get_accuracies(cv_results):
+    """
+    Calculates prediction accuracy for training samples with known outcomes.
+
+    Parameters:
+        cv_results (pandas.DataFrame): predictions for training samples
+
+    Returns:
+        accuracies (pandas.Series): model accuracy w/r to each model type
+    """
+    d_types = cv_results.columns[:-1]
+    accuracies = pd.Series(
+        index=d_types
+    )
+
+    for d_type in d_types:
+        col = cv_results.loc[:, d_type].dropna().astype(int)
+        actual = cv_results.loc[col.index, 'Actual'].astype(int)
+        correct = [1 if col.loc[i] == actual.loc[i] else 0 for i in col.index]
+        accuracies.loc[d_type] = np.mean(correct)
+
+    return accuracies
+
+
+def plot_results(cv_results, cv_probabilities, val_results):
+    """
+    Plots prediction model performance.
+
+    Parameters:
+        cv_results (pandas.DataFrame): predictions for samples with known
+            outcomes
+        cv_probabilities (pandas.DataFrame): predicted probability of
+            persistence for samples with known outcomes
+        val_results (pandas.DataFrame): predictions for samples with unknown
+            outcomes
+
+    Returns:
+        fig (matplotlib.Figure): figure depicting predictions for all samples
+    """
+    fig_size = (10, 8)
+    layout = (3, 2)
+    axs, fig = getSetup(
+        fig_size,
+        layout
+    )
+
+    # Cross-validation Accuracies
+
+    cv_results = cv_results.loc[cv_results['Actual'] != 'Unknown']
+    accuracies = get_accuracies(cv_results)
+    axs[0].bar(
+        range(len(accuracies)),
+        accuracies
+    )
+
+    ticks = [label.replace(' ', '\n') for label in accuracies.index]
+    axs[0].set_xticks(range(len(accuracies)))
+    axs[0].set_xticklabels(ticks, fontsize=10)
+    axs[0].set_ylabel('Mean Accuracy over\n10-fold Cross-Validation', fontsize=12)
+    axs[0].text(
+        0.06,
+        0.96,
+        'A',
+        fontsize=16,
+        fontweight='bold',
+        transform=plt.gcf().transFigure
+    )
+
+    # AUC-ROC Curves
+
+    for d_type in cv_probabilities.columns:
+        col = cv_probabilities.loc[:, d_type].dropna()
+        actual = cv_results.loc[col.index, 'Actual'].astype(int)
+        fpr, tpr, _ = roc_curve(actual, col)
+        axs[1].plot(fpr, tpr)
+
+    axs[1].set_xlim(0, 1)
+    axs[1].set_ylim(0, 1)
+    axs[1].legend(cv_probabilities.columns)
+    axs[1].set_xlabel('False Positive Rate', fontsize=12)
+    axs[1].set_ylabel('True Positive Rate', fontsize=12)
+    axs[1].plot([0, 1], [0, 1], color='k', linestyle='--')
+    axs[1].text(
+        0.49,
+        0.96,
+        'B',
+        fontsize=16,
+        fontweight='bold',
+        transform=plt.gcf().transFigure
+    )
+
+    # Validation set predictions
+
+    axs[2].remove()
+    axs[3].remove()
+    gs = axs[2].get_gridspec()
+    h_map = fig.add_subplot(gs[2:4])
+
+    val_results = val_results.fillna(-1).astype(int)
+    sns.heatmap(
+        val_results.T,
+        ax=h_map,
+        cbar=False,
+        cmap=['dimgrey', '#ffd2d2', '#9caeff'],
+        vmin=-1,
+        linewidths=1
+    )
+
+    h_map.set_xticks(
+        np.arange(0.5, val_results.shape[0], 1)
+    )
+    h_map.set_xticklabels(
+        val_results.index,
+        fontsize=10,
+        rotation=90
+    )
+    h_map.set_yticks(
+        np.arange(0.5, val_results.shape[1], 1)
+    )
+    h_map.set_yticklabels(
+        val_results.columns,
+        fontsize=10,
+        rotation=0
+    )
+    h_map.set_xlabel('Patient', fontsize=12)
+    legend_elements = [Patch(facecolor='dimgrey', edgecolor='dimgrey', label='Data Not Available'),
+                       Patch(facecolor='#ffd2d2', edgecolor='#ffd2d2', label='Persistor'),
+                       Patch(facecolor='#9caeff', edgecolor='#9caeff', label='Resolver')]
+    h_map.legend(handles=legend_elements, loc=[1.02, 0.4])
+    h_map.text(
+        0.06,
+        0.62,
+        'C',
+        fontsize=16,
+        fontweight='bold',
+        transform=plt.gcf().transFigure
+    )
+
+    return fig
+
+
+def export_results(train_samples, train_probabilities, validation_samples):
+    """
+    Reformats prediction DataFrames and saves as .txt.
+
+    Parameters:
+        train_samples (pandas.DataFrame): predictions for training samples
+        train_probabilities (pandas.DataFrame): probabilities of persistence for
+            training samples
+        validation_samples (pandas.DataFrame): predictions for validation samples
+
+    Returns:
+        None
+    """
+    train_samples = train_samples.astype(str)
+    train_probabilities = train_probabilities.astype(str)
+    validation_samples = validation_samples.astype(str)
+
+    validation_samples = validation_samples.replace('0', 'ARMB')
+    validation_samples = validation_samples.replace('1', 'APMB')
+    train_samples = train_samples.replace('0', 'ARMB')
+    train_samples = train_samples.replace('1', 'APMB')
+
+    validation_samples.to_csv(
+        join(
+            PATH_HERE,
+            '..',
+            'output',
+            'validation_predictions.txt'
+        )
+    )
+    train_samples.to_csv(
+        join(
+            PATH_HERE,
+            '..',
+            'output',
+            'train_predictions.txt'
+        )
+    )
+    train_probabilities.to_csv(
+        join(
+            PATH_HERE,
+            '..',
+            'output',
+            'train_probabilities.txt'
+        )
+    )
 
 
 def makeFigure():
-    """ Get a list of the axis objects and create a figure. """
-    # Get list of axis objects
-    ax, f = getSetup((5, 5), (2, 1))
-    pears, cyto = serum_vs_plasma_setup()
-    kines = ["IL-10", "VEGF", "MCP-3", "IL-4", "Fractalkine"]
-    pears = pears[pears[0].isin(kines)]
-    cyto = cyto[cyto["variable"].isin(kines)]
-    a = sns.pointplot(data=pears, x=0, y=1, join=False, ax=ax[0])
-    a.set_xlabel("Cytokine")
-    a.set_ylabel("Pearson's correlation")
-    a.set_title("Serum-Plasma Cytokine Level Correlation")
-    a.set_ylim(0.0, 1.0)
-    b = sns.boxplot(data=cyto, x="variable", y="value", hue="Outcome", ax=ax[1])
-    handles, _ = b.get_legend_handles_labels()
-    b.legend(handles, ["Serum - Resolved", "Serum - Persisted", "Plasma - Resolved", "Plasma - Persisted"])
-    b.set_xlabel("Cytokine")
-    b.set_ylabel("Normalized cytokine level")
-    b.set_title("Normalized Serum Cytokine Level by Outcome")
+    data_types, patient_data = get_data_types()
+    train_samples, train_probabilities = run_cv(data_types, patient_data)
+    validation_samples = run_unknown(data_types, patient_data)
 
-    return f
+    export_results(train_samples, train_probabilities, validation_samples)
+    fig = plot_results(train_samples, train_probabilities, validation_samples)
+
+    return fig

7e7990be97a4a098d21e0bec0284f69886777dba
cztan <cyztan@gmail.com>
2021-09-17 17:02:22 -0700

    Move tensor related to tensorPack

--- tfac/GSEA_preprocessing.py
+++ tfac/GSEA_preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import mygene
 from .dataImport import form_tensor, import_rna
-from .tensor import perform_CMTF
+from tensorPack import perform_CMTF
 from .figures.figureCommon import OPTIMAL_SCALING
 
 path = "tfac/data/mrsa/"

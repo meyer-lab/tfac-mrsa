632a39617b63e8f07734447d30f8a1dc0d934e6a
ctan <cyztan@gmail.com>
2021-11-08 21:53:10 -0800

    Oops, one more in figureCommon

--- tfac/figures/figureCommon.py
+++ tfac/figures/figureCommon.py
@@ -10,7 +10,7 @@ import svgutils.transform as st
 from matplotlib import gridspec, pyplot as plt
 
 from tfac.dataImport import import_cytokines, form_tensor
-from tensorpac import perform_CMTF
+from tensorpack import perform_CMTF
 
 OPTIMAL_SCALING = 32.0
 

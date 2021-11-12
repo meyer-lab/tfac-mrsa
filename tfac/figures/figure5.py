10a6a8f2da5b6dba9906e80f76f77ad60550c83c
Jackson Chin <JacksonLymanChin@gmail.com>
2021-11-11 16:09:02 -0800

    Moved component v. cytokine plots to figure5

--- tfac/figures/figure5.py
+++ tfac/figures/figure5.py
@@ -1,212 +1,128 @@
-from matplotlib.patches import Patch
-from os.path import abspath, dirname, join
+"""
+Creates Figure 5 -- Components vs. Cytokines
+"""
+from os.path import abspath, dirname
+from string import ascii_uppercase
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.preprocessing import scale
+from sklearn.utils import resample
 
-from tfac.figures.figureCommon import getSetup, OPTIMAL_SCALING
-from tfac.dataImport import form_tensor, import_cytokines
-from tfac.predict import predict_known, predict_validation
+from .figureCommon import getSetup, OPTIMAL_SCALING
+from ..dataImport import form_tensor, import_cytokines
+from ..predict import run_model, predict_regression
 from tensorpack import perform_CMTF
 
+N_BOOTSTRAP = 30
 PATH_HERE = dirname(dirname(abspath(__file__)))
+TARGETS = ['status', 'gender', 'race', 'age']
 
 
-def makeFigure():
-    data_types, patient_data = get_data_types()
-    train_samples = run_cv(data_types, patient_data)
-    validation_samples = run_unknown(data_types, patient_data)
-    export_results(train_samples, validation_samples)
-    fig = plot_results(train_samples, validation_samples)
+def tfac_setup():
+    """
+    Import cytokine data and correlate tfac components to cytokines and
+    data sources.
+
+    Parameters:
+        None
+
+    Returns:
+        subjects (pandas.DataFrame): patient correlations to tfac components
+        cytos (pandas.DataFrame): cytokine correlations to tfac components
+        source (pandas.DataFrame): cytokine source correlations to tfac
+            components
+        pat_info (pandas.DataFrame): patient meta-data
+    """
+    tensor, matrix, pat_info = form_tensor(OPTIMAL_SCALING)
+    plasma, _ = import_cytokines()
+    cytokines = plasma.index
+
+    pat_info.loc[:, 'sorted'] = range(pat_info.shape[0])
+    pat_info = pat_info.sort_values(['cohort', 'type', 'status'])
+    sort_idx = pat_info.loc[:, 'sorted']
+    pat_info = pat_info.drop('sorted', axis=1)
+    pat_info = pat_info.T
+
+    factors = perform_CMTF(tensor, matrix)
+    col_names = [f"Cmp. {i}" for i in np.arange(1, factors.rank + 1)]
+    subjects = pd.DataFrame(
+        factors.factors[0][sort_idx, :],
+        columns=col_names,
+        index=[str(x) for x in pat_info.columns]
+    )
+    cytos = pd.DataFrame(
+        factors.factors[1],
+        columns=col_names,
+        index=cytokines
+    )
+    source = pd.DataFrame(
+        factors.factors[2],
+        columns=col_names,
+        index=["Serum", "Plasma"]
+    )
+
+    return subjects, cytos, source, pat_info
+
+
+def plot_results(subjects, cytos, source):
+    """
+    Plots component weights and interpretation.
+
+    Parameters:
+        subjects (pandas.DataFrame): patient correlations to tfac components
+        cytos (pandas.DataFrame): cytokine correlations to tfac components
+        source (pandas.DataFrame): cytokine source correlations to tfac
+            components
+    """
+    fig_size = (5, 5)
+    layout = {
+        'ncols': 2,
+        'nrows': 1,
+        'wspace': 0
+    }
+    axs, fig, _ = getSetup(
+        fig_size,
+        layout,
+        style=None
+    )
+
+    v_min = min(subjects.values.min(), cytos.values.min(), source.values.min())
+    v_max = max(subjects.values.max(), cytos.values.max(), source.values.max())
+
+    sns.heatmap(
+        cytos,
+        cmap="PRGn",
+        center=0,
+        yticklabels=True,
+        cbar=False,
+        vmin=v_min,
+        vmax=v_max,
+        ax=axs[0]
+    )
+    sns.heatmap(
+        source,
+        cmap="PRGn",
+        center=0,
+        yticklabels=True,
+        cbar=False,
+        vmin=v_min,
+        vmax=v_max,
+        ax=axs[1]
+    )
+
+    axs[0].set_yticklabels(cytos.index, fontsize=7)
+    axs[1].set_yticklabels(["Serum", "Plasma"], rotation=0)
+    axs[1].set_xticks(np.arange(0.5, source.shape[1]))
+    axs[1].set_xticklabels([f'Cmp. {i}' for i in range(1, source.shape[1] + 1)])
 
     return fig
 
 
-def export_results(train_samples, validation_samples):
-    """
-    Reformats prediction DataFrames and saves as .csv.
-
-    Parameters:
-        train_samples (pandas.Series): predictions for training samples
-        validation_samples (pandas.Series): predictions for validation samples
-
-    Returns:
-        None
-    """
-    validation_samples = validation_samples.astype(str)
-    train_samples = train_samples.astype(str)
-
-    validation_samples = validation_samples.replace('0', 'ARMB')
-    validation_samples = validation_samples.replace('1', 'APMB')
-    train_samples = train_samples.replace('0', 'ARMB')
-    train_samples = train_samples.replace('1', 'APMB')
-
-    validation_samples.to_csv(
-        join(
-            PATH_HERE,
-            '..',
-            'output',
-            'validation_predictions.txt'
-        )
-    )
-    train_samples.to_csv(
-        join(
-            PATH_HERE,
-            '..',
-            'output',
-            'train_predictions.txt'
-        )
-    )
-
-
-def get_data_types():
-    """
-    Creates data for classification.
-
-    Parameters:
-        None
-
-    Returns:
-        data_types (list[tuple]): data sources and their names
-        patient_data (pandas.DataFrame): patient metadata
-    """
-    plasma_cyto, serum_cyto = import_cytokines()
-    tensor, matrix, patient_data = form_tensor(OPTIMAL_SCALING)
-    patient_data = patient_data.loc[:, ['status', 'type']]
-
-    components = perform_CMTF(tensor, matrix)
-    components = components[1][0]
-
-    data_types = [
-        ('Plasma Cytokines', plasma_cyto.T),
-        ('Plasma IL-10', plasma_cyto.loc['IL-10', :]),
-        ('Serum Cytokines', serum_cyto.T),
-        ('Serum IL-10', serum_cyto.loc['IL-10', :]),
-        ('CMTF', pd.DataFrame(
-            components,
-            index=patient_data.index,
-            columns=list(range(1, components.shape[1] + 1))
-        )
-        )
-    ]
-
-    return data_types, patient_data
-
-
-def run_unknown(data_types, patient_data):
-    """
-    Predicts samples with unknown outcomes.
-
-    Parameters:
-        data_types (list[tuple]): data sources to predict
-        patient_data (pandas.DataFrame): patient metadata
-
-    Returns:
-        predictions (pandas.Series): predictions for each data source
-    """
-    predictions = pd.DataFrame(
-        index=patient_data.index
-    )
-    predictions = predictions.loc[patient_data['status'] == 'Unknown']
-
-    for data_type in data_types:
-        source = data_type[0]
-        data = data_type[1]
-        labels = patient_data.loc[data.index, 'status']
-
-        _predictions = predict_validation(data, labels)
-        predictions.loc[_predictions.index, source] = _predictions
-
-    return predictions
-
-
-def run_cv(data_types, patient_data):
-    """
-    Predicts samples with known outcomes via cross-validation.
-
-    Parameters:
-        data_types (list[tuple]): data sources to predict
-        patient_data (pandas.DataFrame): patient metadata
-
-    Returns:
-        predictions (pandas.Series): predictions for each data source
-    """
-    predictions = pd.DataFrame(
-        index=patient_data.index
-    )
-
-    for data_type in data_types:
-        source = data_type[0]
-        data = data_type[1]
-        labels = patient_data.loc[data.index, 'status']
-
-        _predictions = predict_known(data, labels)
-        predictions.loc[_predictions.index, source] = _predictions
-
-    predictions.loc[:, 'Actual'] = patient_data.loc[:, 'status']
-
-    return predictions
-
-
-def plot_results(cv_results, val_results):
-    """
-    Plots predictions as heatmaps.
-
-    Parameters:
-        cv_results (pandas.DataFrame): predictions for samples with known
-            outcomes
-        val_results (pandas.DataFrame): predictions for samples with unknown
-            outcomes
-
-    Returns:
-        fig (matplotlib.Figure): figure depicting predictions for all samples
-    """
-    fig_size = (8, 8)
-    layout = {
-        'ncols': 1,
-        'nrows': 2
-    }
-    axs, fig, _ = getSetup(
-        fig_size,
-        layout
-    )
-
-    # Cross-validation predictions
-
-    cv_results = cv_results.loc[cv_results['Actual'] != 'Unknown']
-    cv_results = cv_results.fillna(-1).astype(int)
-    axs[0] = sns.heatmap(
-        cv_results.T,
-        ax=axs[0],
-        cbar=False,
-        cmap=['dimgrey', '#ffd2d2', '#9caeff'],
-        vmin=-1
-    )
-
-    axs[0].set_xticks([])
-    axs[0].set_xlabel('Patient', fontsize=12)
-
-    legend_elements = [Patch(facecolor='dimgrey', edgecolor='dimgrey', label='Data Not Available'),
-                       Patch(facecolor='#ffd2d2', edgecolor='#ffd2d2', label='Persistor'),
-                       Patch(facecolor='#9caeff', edgecolor='#9caeff', label='Resolver')]
-    axs[0].legend(handles=legend_elements, loc=[1.05, 0.4])
-
-    # Validation set predictions
-
-    val_results = val_results.fillna(-1).astype(int)
-    sns.heatmap(
-        val_results.T,
-        ax=axs[1],
-        cbar=False,
-        cmap=['dimgrey', '#ffd2d2', '#9caeff'],
-        vmin=-1,
-        linewidths=1
-    )
-
-    axs[1].set_xlabel('Patient', fontsize=12)
-
-    legend_elements = [Patch(facecolor='dimgrey', edgecolor='dimgrey', label='Data Not Available'),
-                       Patch(facecolor='#ffd2d2', edgecolor='#ffd2d2', label='Persistor'),
-                       Patch(facecolor='#9caeff', edgecolor='#9caeff', label='Resolver')]
-    axs[1].legend(handles=legend_elements, loc=[1.05, 0.4])
+def makeFigure():
+    subjects, cytos, source, _ = tfac_setup()
+    fig = plot_results(subjects, cytos, source)
 
     return fig

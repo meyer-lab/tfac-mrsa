09e0b63426f590d882e655962944e3f386294693
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2022-01-26 12:43:07 -0800

    Remove unused average imputation (#173)

--- tfac/impute.py
+++ tfac/impute.py
@@ -109,35 +109,3 @@ def impute_accuracy(missingCube, missingGlyCube, comps, PCAcompare=True):
             PCAR2X[ii] = calcR2X(recon_pca, mIn=imputeMat)
 
     return CMTFR2X, PCAR2X
-
-
-def average_impute(missingCube, missingGlyCube):
-    cube, glyCube, _ = form_tensor()
-    # compare artificially introduced missingness only
-    tIn = np.copy(cube)
-    tIn[np.isfinite(missingCube)] = np.nan
-    mIn = np.copy(glyCube)
-    mIn[np.isfinite(missingGlyCube)] = np.nan
-
-    antigen_impute = np.copy(missingCube)
-    receptor_impute = np.copy(missingCube)
-    impute_glyCube = np.copy(missingGlyCube)
-
-    inds_glyCube = np.where(np.isnan(missingGlyCube))
-    impute_glyCube[inds_glyCube] = np.take(np.nanmean(missingGlyCube, axis=0), inds_glyCube[1])
-    inds_cube = np.where(np.isnan(missingCube))
-    receptor_impute[inds_cube] = np.take(np.nanmean(missingCube, axis=(0, 2)), inds_cube[1])
-    antigen_impute[inds_cube] = np.take(np.nanmean(missingCube, axis=(0, 1)), inds_cube[2])
-
-    def Q2X(tImp):
-        """ Calculate Q2X. For average imputation purpose only. """
-        tMask = np.isfinite(tIn)
-        mMask = np.isfinite(mIn)
-
-        vTop = np.sum(np.square(tImp * tMask - np.nan_to_num(tIn))) + \
-            np.sum(np.square(impute_glyCube * mMask - np.nan_to_num(mIn)))
-        vBottom = np.sum(np.square(np.nan_to_num(tIn))) + np.sum(np.square(np.nan_to_num(mIn)))
-
-        return 1.0 - vTop / vBottom
-
-    return Q2X(receptor_impute), Q2X(antigen_impute)

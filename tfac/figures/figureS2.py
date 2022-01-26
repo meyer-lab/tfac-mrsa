a079c5f121f80600b16f4661ff139f74a5c36152
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2022-01-26 07:42:00 -0800

    Added boxplots to compare cytokine sources (#172)
    Former-commit-id: 36a538f409d452885f95903e8ad456579137fe80

---
+++ tfac/figures/figureS2.py
@@ -0,0 +1,73 @@
+"""
+This creates Figure S2 - Full Cytokine plots
+"""
+import matplotlib
+from matplotlib.patches import Patch
+import numpy as np
+
+from .common import getSetup
+from ..dataImport import import_cytokines
+
+COLOR_CYCLE = matplotlib.rcParams['axes.prop_cycle'].by_key()['color']
+
+
+def makeFigure():
+    """ Get a list of the axis objects and create a figure. """
+    # Get list of axis objects
+    fig_size = (8, 4)
+    layout = {
+        'ncols': 1,
+        'nrows': 1
+    }
+    axs, f, _ = getSetup(
+        fig_size,
+        layout
+    )
+    ax = axs[0]
+
+    plasma, serum = import_cytokines()
+    ax.boxplot(
+        serum.T,
+        positions=np.arange(0, serum.shape[0] * 3, 3),
+        sym='d',
+        patch_artist=True,
+        boxprops={
+            'facecolor': COLOR_CYCLE[0],
+        },
+        flierprops={
+            'markersize': 4,
+            'markerfacecolor': COLOR_CYCLE[0]
+        }
+    )
+    ax.boxplot(
+        plasma.T,
+        positions=np.arange(1, plasma.shape[0] * 3, 3),
+        sym='d',
+        patch_artist=True,
+        boxprops={
+            'facecolor': COLOR_CYCLE[1],
+        },
+        flierprops={
+            'markersize': 4,
+            'markerfacecolor': COLOR_CYCLE[1]
+        }
+    )
+
+    legend_patches = [
+        Patch(color=COLOR_CYCLE[0]),
+        Patch(color=COLOR_CYCLE[1])
+    ]
+    ax.legend(legend_patches, ['Serum', 'Plasma'])
+
+    ax.set_xlim([-2, serum.shape[0] * 3])
+    ax.set_xticks(np.arange(0.5, serum.shape[0] * 3, 3))
+    ax.set_xticklabels(
+        plasma.index,
+        rotation=90,
+        va='top'
+    )
+    ax.set_xlabel('Cytokine')
+    ax.set_ylabel('Normalized Cytokine Level')
+    ax.set_title('Cytokine Level vs. Source')
+
+    return f

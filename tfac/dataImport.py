4c960406221da8f3e49bccdd646493bec579e885
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2022-03-20 20:48:16 -0700

    Run for more iterations (#187)
    Former-commit-id: 2798ec3ca58ba075e5558d4df4be6cbc4cde4765

--- tfac/dataImport.py
+++ tfac/dataImport.py
@@ -170,5 +170,5 @@ def get_factors(variance_scaling: float = OPTIMAL_SCALING, r=8):
             types, and cohort
     """
     tensor, rna, patient_data = form_tensor(variance_scaling)
-    t_fac = perform_CMTF(tensor, rna, r=r, maxiter=200, progress=False)
+    t_fac = perform_CMTF(tensor, rna, r=r, maxiter=800, progress=False)
     return t_fac, patient_data

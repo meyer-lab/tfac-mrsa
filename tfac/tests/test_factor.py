a7c23dce4db34106e4411eed617de8047f01cd8c
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2023-08-03 21:36:45 -0700

    QR orthogonalization of the RNA factors (#208)
    * Moved CMTF to independent file
     - Moves CMTF code to tfac-mrsa cmtf.py
    
    * Added full TPM-RNA dataset, updated import
     - Adds compressed TPM-transformed RNA dataset
     - Updates imports to use TPM measurements instead
    
    * Cleanup
    
    * Increased iterations for CMTF
    
    * Updated scaling parameter to match new RNA dataset
    
    * Seems to be working
    
    * Updated scaling parameter
    
    * Updates persistence-associated components for 2-component models
    
    * Adjusted scaling parameter, removes figureS1 bug
     - Adjusted scaling parameter to match QR
     - Removed boxplot width adjustment in S1
    
    ---------
    
    Co-authored-by: Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
    Co-authored-by: Jackson L. Chin <JacksonLymanChin@gmail.com>
    Former-commit-id: 36deec8e6567ed5332155bbc17be60b0579d9649

--- tfac/tests/test_factor.py
+++ tfac/tests/test_factor.py
@@ -2,11 +2,11 @@
 Test that we can factor the data.
 """
 from ..dataImport import form_tensor
-from tensorpack import perform_CMTF
+from ..cmtf import perform_CMTF
 
 
 def test_CMTF():
     """ Test that we can form the missing tensor. """
     tensor, matrix, _ = form_tensor()
-    tFac = perform_CMTF(tensor, matrix, r=2)
+    tFac = perform_CMTF(tensor, matrix, r=9)
     assert tFac.R2X > 0.0

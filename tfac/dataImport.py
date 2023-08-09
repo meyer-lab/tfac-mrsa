a08030054a4a3fe705c5fc4cb1a0ab9eee590d1e
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2023-08-08 18:27:58 -0700

    Updates CIBERSORTx result, adds options to import  - Updates CIBERSORTx results to use ComBat processed data  - Adds power transform and lm22/cite-seq options to CIBERSORTx import

--- tfac/dataImport.py
+++ tfac/dataImport.py
@@ -177,18 +177,30 @@ def get_factors(variance_scaling: float = OPTIMAL_SCALING, r=8):
     return t_fac, pcaFac, patient_data
 
 
-def get_cibersort_results(power_transform=True):
+def get_cibersort_results(power_transform=True, citeseq=False):
     """
     Returns CIBERSORTx results.
+
+    Parameters:
+        power_transform (bool, default: True): power transform CIBERSORT results
+        citeseq (bool, default: False): return citeseq results instead
 
     Returns:
         cs_results (pandas.DataFrame): CIBERSORTx results
     """
-    cs_results = pd.read_csv(
-        join(PATH_HERE, 'tfac', 'data', 'cibersortx', 'cibersort_lm22.txt'),
-        delimiter='\t',
-        index_col=0
-    )
+    if citeseq:
+        cs_results = pd.read_csv(
+            join(PATH_HERE, 'tfac', 'data', 'cibersortx',
+                 'cibersort_citeseq.txt'),
+            delimiter='\t',
+            index_col=0
+        )
+    else:
+        cs_results = pd.read_csv(
+            join(PATH_HERE, 'tfac', 'data', 'cibersortx', 'cibersort_lm22.txt'),
+            delimiter='\t',
+            index_col=0
+        )
     cs_results = cs_results.loc[cs_results.loc[:, 'P-value'] < 0.01, :]
     cs_results = cs_results.iloc[:, :-3]
 

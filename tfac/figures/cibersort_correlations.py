4186a686b89324a9222f81d1dc82be4e9ac68955
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2023-08-09 13:15:41 -0700

    Reorders cibersort correlations, adds asteriks for significance  - Adds asteriks for cell types with significant correlation to  a component

--- tfac/figures/cibersort_correlations.py
+++ tfac/figures/cibersort_correlations.py
@@ -4,13 +4,13 @@ import pandas as pd
 from scipy.stats import pearsonr, spearmanr
 import seaborn as sns
 
-from tfac.dataImport import get_cibersort_results, get_factors
+from tfac.dataImport import get_cibersort_results, get_factors, reorder_table
 from tfac.figures.common import getSetup
 
 
 def main():
-    cs_results = get_cibersort_results()
-    t_fac, meta = get_factors()
+    cs_results = get_cibersort_results(citeseq=True)
+    t_fac, _, meta = get_factors()
 
     patient_factor = pd.DataFrame(
         t_fac.factors[0],
@@ -23,19 +23,16 @@ def main():
         columns=patient_factor.columns,
         dtype=float
     )
+    p_values = correlations.copy(deep=True)
 
     for cell in cs_results.columns:
         for component in patient_factor.columns:
-            # result = spearmanr(
-            #     patient_factor.loc[:, component],
-            #     cs_results.loc[:, cell]
-            # )
-            # correlations.loc[cell, component] = result.correlation
             corr, p = pearsonr(
                 patient_factor.loc[:, component],
                 cs_results.loc[:, cell]
             )
             correlations.loc[cell, component] = corr
+            p_values.loc[cell, component] = p
 
     fig_size = (5, 5)
     layout = {
@@ -48,17 +45,32 @@ def main():
     )
     ax = axs[0]
 
+    correlations = correlations.dropna(axis=0)
+    correlations = reorder_table(correlations)
     sns.heatmap(
         correlations.astype(float),
         center=0,
-        cmap='PRGn',
+        cmap='coolwarm',
         ax=ax,
         cbar_kws={
             'label': 'Pearson Correlation'
         }
     )
+
     ax.set_yticks(np.arange(0.5, correlations.shape[0]))
     ax.set_yticklabels(correlations.index)
+
+    for xtick, comp in zip(ax.get_xticks(), correlations.columns):
+        for ytick, cell in zip(ax.get_yticks(), correlations.index):
+            if p_values.loc[cell, comp] < 0.05:
+                ax.text(
+                    xtick,
+                    ytick + 0.25,
+                    s='*',
+                    ha='center',
+                    va='center',
+                    ma='center'
+                )
 
     ax.set_ylabel('Cell Type')
     ax.set_xlabel('Component')
